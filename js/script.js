cd5b3c0db762cef00a2a5a25916de0c7cc5c2dfc
Soudea Forbes <soudeaforbes@gmail.com>
2014-10-28 20:31:58 -0400

    new icon

--- js/script.js
+++ js/script.js
@@ -4,26 +4,33 @@ var Thunderforest_Transport = L.tileLayer('http://{s}.tile.thunderforest.com/tra
 	attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 }).addTo(map);
 
-var willoughby = L.marker([40.694491, -73.941721]).addTo(map);
+var myIcon = L.icon({
+    iconUrl: 'http://www.clipartbest.com/cliparts/9c4/e5k/9c4e5kqMi.png',
+    iconSize: [38, 38],
+    iconAnchor: [22, 22],
+    popupAnchor: [-3, -16]
+});
+
+var willoughby = L.marker([40.694491, -73.941721], {icon: myIcon}).addTo(map);
 willoughby.bindPopup("Willoughby");
 
-var flatbush = L.marker([40.640522, -73.950685]).addTo(map);
+var flatbush = L.marker([40.640522, -73.950685], {icon: myIcon}).addTo(map);
 flatbush.bindPopup("Flatbush");
 
-var tibbetts = L.marker([45.500620, -122.646971]).addTo(map);
+var tibbetts = L.marker([45.500620, -122.646971], {icon: myIcon}).addTo(map);
 flatbush.bindPopup("Tibbetts");
 
-var ODB = L.marker([45.478173, -122.630607]).addTo(map);
+var ODB = L.marker([45.478173, -122.630607], {icon: myIcon}).addTo(map);
 ODB.bindPopup("Olde Dorm Block Kanye Westport");
 
-var chittick = L.marker([45.481197, -122.635993]).addTo(map);
+var chittick = L.marker([45.481197, -122.635993], {icon: myIcon}).addTo(map);
 chittick.bindPopup("Chittick");
 
-var macNaughton = L.marker([45.479389, -122.632230]).addTo(map);
+var macNaughton = L.marker([45.479389, -122.632230], {icon: myIcon}).addTo(map);
 macNaughton.bindPopup("MacNaughton II");
 
-var sally = L.marker([45.477208, -122.624054]).addTo(map);
+var sally = L.marker([45.477208, -122.624054], {icon: myIcon}).addTo(map);
 sally.bindPopup("Sally's House");
 
-var OFCS = L.marker([45.492462, -122.630390]).addTo(map);
+var OFCS = L.marker([45.492462, -122.630390], {icon: myIcon}).addTo(map);
 OFCS.bindPopup("OFCS House");
